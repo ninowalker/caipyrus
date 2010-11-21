b6fe8ac553189436fa3135e3599090baa23d408c
Nino Walker <nino.walker@gmail.com>
2010-11-21 13:10:28 -0800

    Install script... doesn't seem to like pycario as a package requirement.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import os.path, sys, subprocess
+
+setup(  name='pypyrus',
+        version='0.1',
+        #packages = ['pypyrus'],
+        install_requires=['pycairo'],
+        zip_safe=True,
+        test_suite='nose.collector',
+        
+        # metadata for upload to PyPI
+        author = "Nino Walker",
+        author_email = "nino@ni-no.info",
+        description = "Cairo based procedural rendering tool.",
+        license = "BSD",
+        keywords = "OSM OpenStreetMap GTFS routing transit",
+        url = "http://github.com/ninowalker/pypyrus/tree/master",
+)
